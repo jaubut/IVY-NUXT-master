d4c70eaf70910078ba6d9a10f01f549b87c342a3
jaubut <jeremie.aubut@gmail.com>
2020-05-06 20:14:19 -0400

    ajout GIF et link

--- pages/index.vue
+++ pages/index.vue
@@ -5,9 +5,17 @@
         <img width="100" src="@/assets/img/logo/fb-live.svg" alt="">
         <h1>Yoga virtuel !</h1>
         <h3>Rejoins-nous sur le <a href="https://www.facebook.com/groups/2350913745213806/" target="_blank"><span style="text-decoration:underline;">groupe Facebook privé</span></a> pour avoir accès à toutes les classes de yoga en ligne.<br>Tu peux te procurer l'accès virtuel pour seulement <a href="https://clients.mindbodyonline.com/classic/ws?studioid=431828&stype=43&prodid=100066" target="_blank"><strong>20 $ par mois</strong></a> ou <a href="https://clients.mindbodyonline.com/classic/ws?studioid=431828&stype=43&prodid=100067" target="_blank"><strong>120 $ pour l'année.</strong></a></h3>
+        <iframe
+          src="https://giphy.com/embed/eJcgZMVLgXItiJTXSK"
+          frameBorder="0"
+          class="video-live-dan"
+          allowFullScreen
+        />
+        <!--
         <video autoplay class="video-live-dan">
           <source type="video/mp4" src="@/assets/videos/fb-live-dan-2.mp4">
         </video>
+        -->
         <p>*Si tu as déjà un abonnement au studio,<br> bonne nouvelle, les classes en ligne sont incluses,<br> tu n'as qu'à demander l'accès <a href="https://www.facebook.com/groups/2350913745213806/" target="_blank"><strong>au groupe</strong></a>!</p>
         <div class="button-group">
           <a href="https://clients.mindbodyonline.com/classic/ws?studioid=431828&stype=43&prodid=100066" target="_blank" class="button-virtuel a">Mensuel 20 $/mois</a>
@@ -93,7 +101,7 @@ export default {
 <style>
 .video-live-dan {
   height: 40vh;
-  width: auto;
+  width: 100%;
   padding-top: 2.5rem;
 }
 .full-fb-live {

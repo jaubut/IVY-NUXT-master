8174747af4931464b7c1262e42eaa83b446f506f
jaubut <jeremie.aubut@gmail.com>
2020-07-01 17:38:31 -0400

    some changes after re opening

--- components/banner-reouverture.vue
+++ components/banner-reouverture.vue
@@ -1,20 +1,21 @@
 <template>
   <div class="container-block">
     <div class="infos-center-v-two" style="margin:2rem 0;">
-      <h1>Réouverture du Studio<br>dès le lundi 29 juin !</h1>
+      <h1>Réouvert<br>depuis le 29 juin !</h1>
       <br>
-      <img src="https://scontent.fymq2-1.fna.fbcdn.net/v/t1.0-9/104682916_891930587953834_2044655645630605935_o.jpg?_nc_cat=104&_nc_sid=110474&_nc_ohc=Ae95h3BbbMYAX8KctU2&_nc_ht=scontent.fymq2-1.fna&oh=ef538fc9528dcb50ca5c8a37d32931ae&oe=5F179B29" style="width:50vw;" alt="">
+      <img src="@/assets/img/yoga/intro.jpg" style="width:50vw;" alt="">
       <br>
       <h3 style="" class="text-cover">
-        <a href="https://clients.mindbodyonline.com/classic/ws?studioid=431828&stype=43&prodid=100069" target="_blank">Yoga illimité tout l’été<br>pour seulement 100 $</a>.<br></br>Plus de 15 cours en Studio, au Parc et en Ligne.
+        <a href="https://clients.mindbodyonline.com/classic/ws?studioid=431828&stype=43&prodid=100023" target="_blank">Mois d'introduction<br>pour seulement 50 $</a>.<br></br>Plus de 15 cours en Studio, au Parc et en Ligne.
       </h3>
+      <br><p>Valable 1 fois par personne.</p>
       <!--
     <video autoplay class="video-live-dan">
         <source type="video/mp4" src="@/assets/videos/fb-live-dan-2.mp4">
     </video>
     -->
       <div class="button-group">
-        <a href="https://clients.mindbodyonline.com/classic/ws?studioid=431828&stype=43&prodid=100069" target="_blank" class="button-virtuel c active">Me procurer l'Été Illimité à 100 $</a>
+        <a href="https://clients.mindbodyonline.com/classic/ws?studioid=431828&stype=43&prodid=100023" target="_blank" class="button-virtuel c active">Me procurer mon offre d'intro pour 50 $</a>
       </div>
     </div>
   </div>

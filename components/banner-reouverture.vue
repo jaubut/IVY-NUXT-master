a7d9e42cc989f8431e03517255ee1ef04b61cc5a
jaubut <jeremie.aubut@gmail.com>
2020-07-13 13:46:52 -0400

    allo reouverture

--- components/banner-reouverture.vue
+++ components/banner-reouverture.vue
@@ -6,7 +6,7 @@
       <img src="@/assets/img/yoga/intro.jpg" style="width:50vw;" alt="">
       <br>
       <h3 style="" class="text-cover">
-        <a href="https://clients.mindbodyonline.com/classic/ws?studioid=431828&stype=43&prodid=100023" target="_blank">Mois d'introduction<br>pour seulement 50 $</a>.<br>(Valable 1 fois par personne.)<br></br>Plus de 15 cours en Studio, au Parc et en Ligne.
+        <a href="https://clients.mindbodyonline.com/classic/ws?studioid=431828&stype=43&prodid=100023" target="_blank">Mois d'introduction<br>pour seulement 50 $</a>.<br>(Valable 1 fois par personne.)<br></br>Plus de 10 cours en Studio, au Parc et en Ligne.
       </h3>
       <!--
     <video autoplay class="video-live-dan">

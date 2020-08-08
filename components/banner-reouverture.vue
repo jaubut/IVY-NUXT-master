a0dab4c5a719ad431233ec2905bc66a23d9e9662
jaubut <jeremie.aubut@gmail.com>
2020-08-08 11:43:46 -0400

    update de banner retour a intro

--- components/banner-reouverture.vue
+++ components/banner-reouverture.vue
@@ -1,20 +1,12 @@
 <template>
   <div class="container-block">
     <div class="infos-center-v-two" style="margin:2rem 0;">
-      <h1>Promo<br>4 semaines à 40 $ !</h1>
+      <h1>Promo<br>d'intro à 40 $ !</h1>
       <br>
       <img src="@/assets/img/yoga/intro.jpg" style="width:50vw;" alt="">
       <br>
       <h3 style="" class="text-cover">
-<<<<<<< HEAD
-<<<<<<< HEAD
-        Valable jusqu'au 7 Août seulement<br/>12 cours en Studio, au Parc et en Ligne.
-=======
-        <a href="https://clients.mindbodyonline.com/classic/ws?studioid=431828&stype=43&prodid=100023" target="_blank">Mois d'introduction<br>pour seulement 50 $</a>.<br>(Valable 1 fois par personne.)<br></br>Plus de 15 cours en Studio, au Parc et en Ligne.
->>>>>>> parent of a7d9e42... allo reouverture
-=======
-        <a href="https://clients.mindbodyonline.com/classic/ws?studioid=431828&stype=43&prodid=100023" target="_blank">Mois d'introduction<br>pour seulement 50 $</a>.<br>(Valable 1 fois par personne.)<br></br>Plus de 15 cours en Studio, au Parc et en Ligne.
->>>>>>> parent of a7d9e42... allo reouverture
+        <a href="https://clients.mindbodyonline.com/classic/ws?studioid=431828&stype=43&prodid=100023" target="_blank">Mois d'introduction<br>pour seulement 50 $</a>.<br>(Valable 1 fois par personne.)<br/>Plus de 15 cours en Studio, au Parc et en Ligne.
       </h3>
       <!--
     <video autoplay class="video-live-dan">

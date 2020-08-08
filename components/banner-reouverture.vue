988d2506568370e34c5255a2a51c4a87aea94661
jaubut <jeremie.aubut@gmail.com>
2020-08-08 12:54:33 -0400

    correction de syntax

--- components/banner-reouverture.vue
+++ components/banner-reouverture.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="container-block">
     <div class="infos-center-v-two" style="margin:2rem 0;">
-      <h1>Mois<br>d'introduction pour seulement 50 $</h1>
+      <h1>Mois d'introduction<br>pour seulement 50 $</h1>
       <br>
       <img src="@/assets/img/yoga/intro.jpg" style="width:50vw;" alt="">
       <br>
